974dcbae38511f1c8d77ef1ffdaaf4db899d57aa
immortals-ume <kaish127799@outlook.com>
2022-12-27 12:36:58 +0530

    Initial Project structure is created

---
+++ src/main/java/com/immortals/pollingappbackend/PollingAppBackendApplication.java
@@ -0,0 +1,14 @@
+package com.immortals.pollingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingAppBackendApplication{
+
+
+    public static void main( String[] args ){
+        SpringApplication.run( PollingAppBackendApplication.class,args );
+    }
+
+}
---
+++ src/test/java/com/immortals/pollingappbackend/PollingAppBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.immortals.pollingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingAppBackendApplicationTests{
+
+
+    @Test
+    void contextLoads(){
+    }
+
+}
